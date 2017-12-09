31b2f5f97015a96d57d55b3b4a42d53b98da5faa
alan <ahmadjalaluddin4869@gmail.com>
2017-12-09 20:20:05 +0700

    cobaLogin

---
+++ app/src/main/java/com/example/android/futsalkuy/BackgroundWorker.java
@@ -0,0 +1,16 @@
+package com.example.android.futsalkuy;
+
+import android.content.Context;
+import android.os.AsyncTask;
+
+/**
+ * Created by febi on 09/12/2017.
+ */
+
+public abstract class BackgroundWorker extends AsyncTask<Void,Void,Void> {
+    Context context;
+    BackgroundWorker (Context ctx){
+
+    }
+
+}
--- app/src/main/java/com/example/android/futsalkuy/Login.java
+++ app/src/main/java/com/example/android/futsalkuy/Login.java
@@ -4,17 +4,28 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
 
 public class Login extends AppCompatActivity {
+
+    EditText txtUsername, txtPassword;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        txtPassword = (EditText) findViewById(R.id.password);
+        txtUsername = (EditText) findViewById(R.id.username);
     }
 
     public void registerNow(View view){
         Intent i = new Intent(this, Register.class);
         startActivity(i);
+    }
+
+    public void Login(){
+        String username = txtUsername.getText().toString();
+        String password = txtPassword.getText().toString();
     }
 }
